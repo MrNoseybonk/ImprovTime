5cfd710e4d64ed7ef959b3e81afbe01451107560
Steven Wilson <speedy0097@yahoo.com>
2020-07-14 11:18:15 -0400

    Adding Angular project. Basic skeleton of the project.

---
+++ ImprovTime/ImprovTime/src/app/Components/improv/improv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-improv',
+  templateUrl: './improv.component.html',
+  styleUrls: ['./improv.component.css']
+})
+export class ImprovComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ImprovTime/ImprovTime/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ImprovComponent } from './Components/improv/improv.component';
+import { MainNavBarComponent } from './Components/main-nav-bar/main-nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ImprovComponent,
+    MainNavBarComponent
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
